79a7dfbc7b82b99a5742e6d34562c9ee300f1c05
ichiro.M <apotikisin4632@gmail.com>
2019-08-28 10:45:55 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  #Sign up
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = '新規登録に成功しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = '新規登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   #トップページ
   root to: 'toppages#index'
   
+  #Signup
+  get 'signup', to: 'users#new'
+  
+  resources :users, only: [:show, :new, :create]
 end
